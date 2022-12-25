2243d09811911bfad0f43ab6f2afa7a6f0ea27db
Ahmet Can Gumrukcu <a.c.gumrukcu@gmail.com>
2022-12-25 14:58:35 +0300

    add spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Header from "./components/header"
+import Header from "./components/header";
 import SearchBar from "./components/searchBar";
+import Spinner from "react-bootstrap/Spinner";
 
 const options = {
   method: "GET",
@@ -24,8 +25,17 @@ function App() {
       "https://imdb8.p.rapidapi.com/auto-complete?q=" + val + "",
       options
     )
-      .then((response) => response.json())
-      .then((response) => setMovie(response.d))
+      .then((response) => {
+        setLoading(true);
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        setTimeout(() => {
+          setLoading(false);
+          return setMovie(response.d);
+        }, 4000);
+      })
       .catch((err) => console.error(err));
   };
 
@@ -33,31 +43,37 @@ function App() {
     search();
     setLoading(false);
   }, []);
-  
 
   return (
     <div className="App">
-      <Header/>
-      <Container> 
-        {<SearchBar search={search}/> } 
+      <Header />
+      <Container>
+        {<SearchBar search={search} />}
         <Row>
-          {loading && !errorMessage && <span>loading...</span>}
+          {loading && !errorMessage && <span>Loading...</span>}
           {movie &&
             movie.map((val, index) => {
               return (
                 <Col xs={4} md={3} key={index} className="mb-4">
-                  <Card>
-                    <Card.Img
-                      height="250px"
-                      variant="top"
-                      src={val.i ? val.i.imageUrl : ""}
-                    />
-                    <Card.Body>
-                      <Card.Title>{val.l}</Card.Title>
-                      <Card.Text>{val.s}</Card.Text>
-                      <Card.Text>Type: {val.q}</Card.Text>
-                      <Card.Text>Date: {val.y ? val.y : "Unknown"}</Card.Text>
-                    </Card.Body>
+                  <Card className="d-flex align-items-center justify-content-center" style={{ minHeight: "30rem" }}>
+                    {loading && <Spinner animation="border" />}
+                    {!loading && (
+                      <Fragment>
+                        <Card.Img
+                          height="250px"
+                          variant="top"
+                          src={val.i ? val.i.imageUrl : ""}
+                        />
+                        <Card.Body>
+                          <Card.Title>{val.l}</Card.Title>
+                          <Card.Text>{val.s}</Card.Text>
+                          <Card.Text>Type: {val.q}</Card.Text>
+                          <Card.Text>
+                            Date: {val.y ? val.y : "Unknown"}
+                          </Card.Text>
+                        </Card.Body>
+                      </Fragment>
+                    )}
                   </Card>
                 </Col>
               );
